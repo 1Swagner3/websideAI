afd0d0092515bfa38aea3c3fc451c263d3369896
WASASCH <sascha.wagner@ext.experienceone.com>
2023-10-06 13:06:19 +0200

    chat component changes

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from websideAI import websideAI # Import the function that runs your AI
+from websideAI import websideAI
 
 app = Flask(__name__)
 CORS(app)
