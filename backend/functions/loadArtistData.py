c3faf9e09f56f842e94e089262cf6be955eabed6
1Swagner3 <84774754+1Swagner3@users.noreply.github.com>
2023-10-06 10:37:44 +0200

    moving files into backend folder

--- backend/functions/loadArtistData.py
+++ backend/functions/loadArtistData.py
@@ -4,11 +4,11 @@ from functions.albumToText import json_to_text
 
 def load_artist_data():
     # Read biography
-    with open("./artistData/biography.txt", "r") as file:
+    with open("backend/artistData/biography.txt", "r") as file:
         biography = file.read()
 
     # Read and convert lyrics JSON to text
-    with open("./artistData/album.json", "r") as file:
+    with open("backend/artistData/album.json", "r") as file:
         lyrics_data = json.load(file)
         lyrics = json_to_text(lyrics_data)
 
