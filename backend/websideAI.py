c8e2792669b0b9a216ab43a3f65d21718612b80f
1Swagner3 <84774754+1Swagner3@users.noreply.github.com>
2023-10-13 15:34:07 +0200

    remove quit command from start script

--- backend/websideAI.py
+++ backend/websideAI.py
@@ -5,9 +5,6 @@ from controller.controller import validate_user_input
 def websideAI(user_query):
     print("Starting AI")
 
-    if user_query.lower() in ["exit", "quit"]:
-        return "Goodbye!"
-
     response = validate_user_input(user_query)
 
     return response
